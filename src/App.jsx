d2cd07ef5df4ba61f32a51aa1b88dde0ab424ab9
Kamrul Hassan <kamrulthedev@gmail.com>
2023-10-03 20:59:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className=' text-6xl bg-red-800 font-bold '>Knowledge Cafe</h1>
+      <h2 className='font-bold text-6xl mt-12'>KAMRUL HASSAN</h2>
+       
+       
+    </>
+  )
+}
+
+export default App
