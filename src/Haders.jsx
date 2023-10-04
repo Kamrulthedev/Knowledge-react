7c03f6076839fd9e50dee1c6fc3344b548031c1d
Kamrul Hassan <kamrulthedev@gmail.com>
2023-10-04 09:09:50 +0600

    fure-commit

--- src/Haders.jsx
+++ src/Haders.jsx
@@ -1,10 +1,10 @@
-
+import profile from '../src/assets/images/profile.png'
 
 const Haders = () => {
     return (
-        <div>
+        <div className='flex justify-between items-center'>
             <h1 className=' text-6xl font-bold '>Knowledge Cafe</h1>
-            
+            <img src={profile} alt="" />
         </div>
     );
 };
