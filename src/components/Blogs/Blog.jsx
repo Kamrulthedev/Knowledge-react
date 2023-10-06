8e6f3882393c3a42320d593f1e54748920d91750
Kamrul Hassan <kamrulthedev@gmail.com>
2023-10-06 12:03:27 +0600

    book-marek-commit

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,11 +1,12 @@
 
 import PropTypes from 'prop-types'; 
+import { FaBookmark } from 'react-icons/fa';
 
 const Blog = ({blog}) => {
     const{title, cover,author,hashtags,author_img,reading_time,posted_date}= blog;
     return (
         <div>
-            <img className='w-80 ml-24 rounded-lg' src={cover} alt={`Cover picture of the title ${title}`} />
+            <img className='w-96 ml-24 rounded-lg' src={cover} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between mt-8'>
                 <div className='flex'>
                     <img className='w-14 ml-24' src={author_img} alt="" />
@@ -15,7 +16,7 @@ const Blog = ({blog}) => {
                     </div>
                 </div>
                 <div>
-                    <span className='ml-24'>{reading_time}min read</span>
+                    <span className='ml-24 font-bold'>{reading_time}min read</span><button className='ml-2 text-red-700 text-xl'><FaBookmark></FaBookmark></button>
                 </div>
             </div>
             <h2 className="text-2xl ml-24 m-4 font-bold">{title}</h2>
